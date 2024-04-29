2e44c33a9a183924104c45997b60c040028bc8b6
Vitalii <vetali5700gmail.com>
2024-04-29 22:10:14 +0300

    add models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,32 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("name", )
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    datetime = models.DateTimeField(auto_now=True)
+    deadline = models.DateTimeField()
+    done = models.BooleanField()
+    # Many-to-many
+    authors = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="tasks")
+
+    class Meta:
+        ordering = ("done", "datetime",)
+
+    def __str__(self):
+        return f"{self.content} (created: {self.datetime}, done status: {self.done})"
+
+    def get_absolute_url(self):
+        return reverse("tasks:task-detail", args=[str(self.id)])
