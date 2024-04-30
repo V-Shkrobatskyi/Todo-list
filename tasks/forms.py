b7b5c67fc77d599b462c888032a46492a5fb33b8
Vitalii <vetali5700gmail.com>
2024-04-30 22:18:22 +0300

    update deadline field in TaskForm in forms.py

--- tasks/forms.py
+++ tasks/forms.py
@@ -3,18 +3,14 @@ from tasks.models import Task, Tag
 
 
 class TaskForm(forms.ModelForm):
-    # deadline = forms.DateTimeField(required=False)
-    # deadline = forms.DateTimeInput()
-    # deadline = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'],
-    #                                widget=forms.DateTimeInput(
-    #                                    format='%d/%m/%Y %H:%M',
-    #                                    attrs={'class': 'form-control', 'id': 'datetimepicker'}
-    #                                )
-    #                                )
-    # deadline = forms.TextInput(attrs={'type': 'datetime-local'})
-    # deadline = forms.DateTimeInput(attrs={'class': 'form-control', 'data - target': '  # datetimepicker1'})
-    # deadline = forms.DateTimeField(input_formats=['%d/%m/%y %H:%M'])
-    deadline = forms.DateField(widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'date'}))
+    deadline = forms.DateTimeField(
+        input_formats=['%d:%m:%Y %H:%M'],
+        widget=forms.DateTimeInput(
+            format='%d:%m:%Y %H:%M',
+        ),
+        required=False,
+    )
+
     tags = forms.ModelMultipleChoiceField(
         queryset=Tag.objects.all(),
         widget=forms.CheckboxSelectMultiple,
